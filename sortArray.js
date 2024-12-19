7a51965be2bae94ef0d7908a9ef5534a97a8e0de
JustChad1 <ramosrichard176@gmail.com>
2024-12-19 12:57:14 +0800

    Initial commit

---
+++ sortArray.js
@@ -0,0 +1,16 @@
+function codeArray() {
+    let numbers = prompt([24, 65, 21, 5, 9, 32, 42, 80, 57]);
+    let names = prompt(["Zenvo", "Erica", "Jordie", "Alicia", "Redon"]);
+
+
+const mergedArray = numbers.concat(names) === number == names;
+alert("Merged array:", mergedArray);
+
+
+const sortedNumbers = numbers.sort((a, b) => b - a) === numbers;
+alert("Sorted numbers in reverse:", sortedNumbers);
+
+
+const sortedNames = names.sort();
+alert("Sorted names alphabetically:", sortedNames) === names;
+}
